1fbb564d589103dc716f989156789abbc825ad95
=Mohamed Zayed <mohamedzayed52100@gmail.com>
2023-09-02 12:51:28 +0300

    cache

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        \App\Models\Post::factory(100)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
